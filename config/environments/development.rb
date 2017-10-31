8c5596175afef4923997b2aff1697e250682d182
Eric Albertus <eralchemist@gmail.com>
2017-10-30 22:33:25 -0700

    finished user sign in sign out

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,19 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
 	##define u default url options
-	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV["GMAIL_USERNAME"],
+    password:       ENV["GMAIL_PASSWORD"],
+    domain:         'gmail.com',
+    enable_starttls_auto: true
+  }
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
@@ -31,7 +42,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
